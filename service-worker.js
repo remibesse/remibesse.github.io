9497b7977a2a50e508857cef2aa4acb3f48cc030
Victoire <38075311+Victoire44@users.noreply.github.com>
2022-08-29 19:07:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1f40e59c4226a8b93e838879443a98c.js"
+  "/precache-manifest.31321f900970910a163e124a710c906a.js"
 );
 
 self.addEventListener('message', (event) => {
